0f62688c4cd2649cf77de63fd8446521b200d940
Malorod <malorod99@mail.ru>
2021-08-09 00:04:22 +0300

    annotation config added

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,11 +1,12 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.*;
 
-// Stub
+@Repository
 public class PostRepository {
   private static Map<Long, Post> db = new HashMap<>();
   private static long allPostsCounter = 0;
