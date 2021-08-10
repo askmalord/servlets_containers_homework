42b4492cd95a0f127167883027b45a236f8e3d56
Malorod <malorod99@mail.ru>
2021-08-11 00:23:56 +0300

    changes in MainServlet class added

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,12 +1,60 @@
 package ru.netology.servlet;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.netology.service.PostService;
+import ru.netology.controller.PostController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static java.lang.Long.parseLong;
 
 
-public class MainServlet {
-  public static void main(String[] args) {
-//    final var context = new AnnotationConfigApplicationContext("ru.netology");
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+
+  @Override
+  public void init() {
+    final var context = new AnnotationConfigApplicationContext("ru.netology");
+    controller = (PostController) context.getBean("postController");
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    final var path = req.getRequestURI();
+    if (path.equals("/api/posts")) {
+      controller.all(resp);
+      return;
+    }
+    if (path.matches("/api/posts/\\d+")) {
+      final var id = parseLong(path.substring(path.lastIndexOf("/")));
+      controller.getById(id, resp);
+      return;
+    }
+    super.doGet(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    final var path = req.getRequestURI();
+    if (path.equals("/api/posts")) {
+      controller.save(req.getReader(), resp);
+      return;
+    }
+    super.doPost(req, resp);
+  }
+
+  @Override
+  protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    final var path = req.getRequestURI();
+    if (path.matches("/api/posts/\\d+")) {
+      final var id = parseLong(path.substring(path.lastIndexOf("/")));
+      controller.removeById(id, resp);
+      return;
+    }
+    super.doDelete(req, resp);
   }
 }
 
